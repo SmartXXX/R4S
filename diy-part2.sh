43a94ca539b46f6930de0dea0f059eed1ca9f3d4
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2022-02-20 16:53:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Add a feed source
-svn co https://github.com/esirplayground/luci-app-poweroff package/lean/luci-app-poweroff
-svn co https://github.com/ujincn/luci-app-smartdns-compat package/lean/luci-app-smartdns
-svn co https://github.com/ujincn/smartdns package/lean/smartdns
+svn co https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+svn co https://github.com/ujincn/smartdns package/smartdns
+svn co https://github.com/ujincn/luci-app-smartdns-compat package/luci-app-smartdns
