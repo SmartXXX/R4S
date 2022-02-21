b3808e970a29781b13c0341e56a4448c923823b7
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2022-02-21 23:19:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='SmartR4S''
 # Add luci-theme-argon
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-argon-config package/lean/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Add luci-app-poweroff
