c338cf0b988a876f2a59708ce43f88616efc7ead
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2022-02-22 16:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,7 @@
 # Modify default IP & hostname
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='SmartR4S'' package/lean/default-settings/files/zzz-default-settings
-
-# 修改root密码
-#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:Smart318/g' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Smart R4S /g" package/lean/default-settings/files/zzz-default-settings
 
 # Add luci-theme-argon
 rm -rf package/lean/luci-theme-argon
@@ -32,3 +30,9 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git package/lean/
 
 # Add luci-app-smartdns
 svn co https://github.com/281677160/openwrt-package/trunk/feeds/luci/applications/luci-app-smartdns package/lean/luci-app-smartdns
+
+# from DHDAXCW
+rm -rf ./package/kernel/linux/modules/video.mk
+wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/master/package/kernel/linux/modules/video.mk
+wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3399/base-files/etc/init.d/fa-rk3399-pwmfan
+wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3399/base-files/usr/bin/start-rk3399-pwm-fan.sh
