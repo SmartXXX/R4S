cb8b37b547070f2716b53012e1f22434bda6468f
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2022-02-24 13:01:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,9 +15,6 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='SmartR4S'' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt /SmartR4S /g' package/lean/default-settings/files/zzz-default-settings
 
-# 添加防火墙规则
-sed -i '$a echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> /etc/firewall.user' package/lean/default-settings/files/zzz-default-settings
-
 # Add luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/lean/luci-theme-argon
