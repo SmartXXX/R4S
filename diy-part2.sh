28e9769797454e640da3df6a93b6ab77bbcadd30
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2023-09-11 13:32:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,7 +54,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='SmartR4S'' pack
 sed -i 's/OpenWrt /SmartR4S /g' package/lean/default-settings/files/zzz-default-settings
 
 # 禁用ipv6前缀
-# sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+# sed -i 's/^[^#].*option ula/#&/' package/base-files/files/etc/config/network
 
 # Add firewall rules
 echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> package/network/config/firewall/files/firewall.user
