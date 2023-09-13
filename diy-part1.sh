a82d46f7b89c001c463c08b708af6a7a0b95cc76
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2023-09-13 08:38:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,3 @@
 # echo 'src-git helloworld https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/kenzok8/small.git' >>feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
-
-echo 'src-git passwall https://github.com/ysx88/openwrt-packages/trunk/openwrt-passwall-packages' >>feeds.conf.default
-svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-passwall feeds/passwall/luci-app-passwall
-svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-passwall2 feeds/passwall/luci-app-passwall2
-
-echo 'src-git helloworld https://github.com/ysx88/openwrt-packages/trunk/helloworld' >>feeds.conf.default
-svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-ssr-plus feeds/helloworld/luci-app-ssr-plus
