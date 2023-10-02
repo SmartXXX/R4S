b70c58b32dbe2c6afe7498a6d18ec0a5a6df3c07
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2023-10-02 17:48:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -118,8 +118,8 @@ svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-smartdns pac
 # svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/lean/luci-app-passwall
 # svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-passwall feeds/lienol/luci-app-passwall
 # svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/lean/luci-app-passwall
-# svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-passwall2 package/lean/luci-app-passwall2
-# svn export --force https://github.com/ysx88/openwrt-packages/trunk/openwrt-passwall package/lean
+svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-passwall2 package/lean/luci-app-passwall2
+svn export --force https://github.com/ysx88/openwrt-packages/trunk/openwrt-passwall package/lean
 
 # Add luci-app-ssr-plus
 # rm -rf feeds/helloworld/luci-app-ssr-plus
@@ -127,8 +127,8 @@ svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-smartdns pac
 # svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 
 # Add luci-app-vssr
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
+# git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
 # svn export https://github.com/kenzok8/openwrt-packages/trunk/lua-maxminddb package/lean/lua-maxminddb
 # svn export https://github.com/281677160/openwrt-package/trunk/luci-app-vssr package/lean/luci-app-vssr
 # svn export https://github.com/ysx88/openwrt-packages/trunk/lua-maxminddb package/lean/lua-maxminddb
