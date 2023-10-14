59ff89e929a27297c569b8b9d48ca5bf6eeda1a5
SmartXXX <99791202+SmartXXX@users.noreply.github.com>
2023-10-14 14:16:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -62,7 +62,7 @@ echo 'iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE' >> package/network/c
 echo 'iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport 5001 -j DNAT --to-destination 192.168.6.6:5001' >> package/network/config/firewall/files/firewall.user
 echo 'iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport 6690 -j DNAT --to-destination 192.168.6.6:6690' >> package/network/config/firewall/files/firewall.user
 echo 'iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport 8085 -j DNAT --to-destination 192.168.6.6:8085' >> package/network/config/firewall/files/firewall.user
-echo 'iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport 5006 -j DNAT --to-destination 192.168.6.6:5006' >> package/network/config/firewall/files/firewall.user
+echo 'iptables -t nat -I PREROUTING -i pppoe-wan -p tcp --dport 9080 -j DNAT --to-destination 192.168.6.6:9080' >> package/network/config/firewall/files/firewall.user
 sed -i 's/-j REDIRECT --to-ports 53/-j REDIRECT --to-ports 6153/g' package/lean/default-settings/files/zzz-default-settings
 
 # modify Lienol's Packages
